361430f09dd7ddf3245c5a3302154acc470685c0
我爱果汁 <1223497753@qq.com>
2022-08-28 21:29:26 +0800

    4th commit

---
+++ src/main/java/entity/Pet.java
@@ -0,0 +1,15 @@
+package entity;
+
+public class Pet {
+
+    String name;
+    int age;
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/springframework/beans/factory/PropertyValues.java
@@ -0,0 +1,35 @@
+package springframework.beans.factory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PropertyValues {
+    private final List<PropertyValue> propertyValueList = new ArrayList<>();
+
+    public PropertyValues(PropertyValue... pvs) {
+        addPropertyValue(pvs);
+    }
+
+    public void addPropertyValue(PropertyValue pv) {
+        propertyValueList.add(pv);
+    }
+
+    public void addPropertyValue(PropertyValue... pvs) {
+        propertyValueList.addAll(Arrays.asList(pvs));
+    }
+
+
+    public PropertyValue[] getPropertyValues() {
+        return propertyValueList.toArray(new PropertyValue[0]);
+    }
+
+    public PropertyValue getPropertyValue(String name) {
+        for (PropertyValue propertyValue : propertyValueList) {
+            if (propertyValue.getName().equals(name)) {
+                return propertyValue;
+            }
+        }
+        return null;
+    }
+}
